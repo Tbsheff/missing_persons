d197c5e084b9ed656a889d6c477a81af3449b7c9
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-04-01 11:25:35 -0600

    More resources

--- trafficking/urls.py
+++ trafficking/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import indexPageView, chartPageView, aboutPageView
+from .views import indexPageView, chartPageView, aboutPageView, CTDCPageView, ToolkitPageView, HumanTraffickingHotlinePageView, UtahReportPageView, StatisticsPageView, UTAPageView, AttorneyGeneralPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("charts/", chartPageView, name="charts"),
     path("about/", aboutPageView, name="about"),
+    path("CTDC/", CTDCPageView, name="CTDC"),
+    path("toolkit/", ToolkitPageView, name="toolkit"),
+    path("report/", UtahReportPageView, name="report"),
+    path("hth/", HumanTraffickingHotlinePageView, name="hth"),
+    path("stats/", StatisticsPageView, name="stats"),
+    path("uta/", UTAPageView, name="UTA"),
+    path("attorney/", AttorneyGeneralPageView, name="attorney"),
 ]
