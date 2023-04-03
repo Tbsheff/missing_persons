a10d2f52a4ef1cf5fc5096c8e9fd420ca12c7aa4
Tyler Sheffield <tylersheffield427@gmail.com>
2023-04-03 17:46:34 -0600

    commit

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ja%)6tl+zd%d^1y765nya8o=wgrz%fua5=9hjt=7(la*tn2k$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
