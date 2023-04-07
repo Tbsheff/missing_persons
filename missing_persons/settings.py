3e679569f3616148178b211cbeb7ff7e9b8314a4
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-04-06 19:07:27 -0600

    models

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missingpeople',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'Arrows12!',
         'HOST': 'localhost'
     }
 }  
