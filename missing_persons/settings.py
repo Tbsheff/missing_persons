be36413893ccd5a9def225dcd80f693ea5e81112
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-04-11 12:35:03 -0600

    Password change

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from getpass import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missingpeople',
         'USER': 'postgres',
-        'PASSWORD': 'Hello',
+        'PASSWORD': getpass(),
         'HOST': 'localhost',
         'PORT': '5432'
     }
