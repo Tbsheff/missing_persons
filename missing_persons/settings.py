09db876001a0437b501038175e37d574f7e55ab8
Tyler Sheffield <tylersheffield427@gmail.com>
2023-04-11 13:02:29 -0600

    commit

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -16,16 +16,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from getpass import getpass
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ja%)6tl+zd%d^1y765nya8o=wgrz%fua5=9hjt=7(la*tn2k$&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
