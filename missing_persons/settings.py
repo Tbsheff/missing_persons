5b7a77f64d7fd2bd14eb954b7a00544cee66d2b6
bbundy12 <105405942+bbundy12@users.noreply.github.com>
2023-04-11 17:11:30 -0600

    update

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-ja%)6tl+zd%d^1y765nya8o=wgrz%fua5=9hjt=7(la*tn2k$&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
